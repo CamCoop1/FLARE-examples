f904ed8cc95201626405a760e245c0f2baf5bb88
Cameron Cooper Harris <cameron.harris@adelaide.edu.au>
2026-03-03 10:55:28 +1030

    Removed old analysis folder from repo

--- analysis/studies/fastsim_example_with_multi_detector_MC/mc_production/setup.sh
+++
@@ -1,2 +0,0 @@
-source /cvmfs/sw.hsf.org/spackages7/key4hep-stack/2023-04-08/x86_64-centos7-gcc11.2.0-opt/urwcv/setup.sh
-source /remote/nas00-1/users/charris/phd/fcc/FLARE-examples/.venv/bin/activate
